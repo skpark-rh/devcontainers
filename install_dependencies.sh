c38e2f0ecba16ab60758fef88d963da5f525ee0d
Samuel Park <sampark@redhat.com>
2026-03-02 20:26:34 +0000

    Added an installation script.

---
+++ install_dependencies.sh
@@ -0,0 +1,11 @@
+# install miniconda
+curl -LO "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
+bash Miniforge3-Linux-x86_64.sh -p $HOME/miniconda -b
+rm Miniforge3-Linux-x86_64.sh && export PATH="$HOME/miniconda/bin:$PATH"
+conda update -y conda && conda init && conda install -c anaconda -y python=3.10 gdb cuda-gdb
+
+# install pytorch dependencies
+cd pytorch && pip install -r requirements.txt mkl-include mkl-static
+
+# install ray dependencies
+pip install ray[all]
