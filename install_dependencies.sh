c07eb997d8cb3bc756588871ed239f6cd18efd65
Samuel Park <sampark@redhat.com>
2026-03-02 22:10:06 +0000

    Add source to install packages in conda env.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,6 +3,7 @@ curl -LO "https://github.com/conda-forge/miniforge/releases/latest/download/Mini
 bash Miniforge3-Linux-x86_64.sh -p $HOME/miniconda -b
 rm Miniforge3-Linux-x86_64.sh && export PATH="$HOME/miniconda/bin:$PATH"
 conda update -y conda && conda init && conda install -c anaconda -y python=3.10 gdb cuda-gdb
+source ~/.bashrc
 
 # install pytorch dependencies
 cd pytorch && pip install -r requirements.txt mkl-include mkl-static
