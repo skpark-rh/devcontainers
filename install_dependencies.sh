00005819a56d3cab8355ba77e34d5a7980c7555d
Samuel Park <sampark@redhat.com>
2026-03-04 19:56:50 +0000

    Added a use_cuda func and changed the repo pull to a static link for quay.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,9 +12,15 @@ export MAX_JOBS=25\
 export CC=gcc\
 export CXX=g++\
 export CMAKE_PREFIX_PATH=/home/sampark/miniconda\
-export NVCC_CCBIN=g++\
-export CUDA_HOME=/usr/local/cuda-12.8\
-export PATH=/usr/local/cuda-12.8/bin:$PATH' ~/.bashrc
+export NVCC_CCBIN=g++' ~/.bashrc
+
+# add use-cuda function to .bashrc
+echo '
+use_cuda() {
+    export PATH=$(echo "$PATH" | tr '"'"':'"'"' '"'"'\n'"'"' | grep -v '"'"'/usr/local/cuda'"'"' | tr '"'"'\n'"'"' '"'"':'"'"' | sed '"'"'s/:$//'"'"')
+    export PATH=/usr/local/cuda-$1/bin:$PATH
+    export CUDA_HOME=/usr/local/cuda-$1
+}' >> ~/.bashrc
 
 source ~/.bashrc
 
